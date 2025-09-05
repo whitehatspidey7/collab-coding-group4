004295f19e2933c8e626011d06bf6e40aa7684d0
uday <udayasu.2004@gmail.com>
2025-09-05 20:27:13 +0530

    added .cpp extention to test_fibonacci file

--- main.cpp
+++ main.cpp
@@ -60,23 +60,4 @@ int main() {
     cout << "Fibonacci(7): " << fibonacci(7) << endl;
 
     return 0;
-//>>>>>>> main
-}
-
-int main() {
-    cout << "Driver Program - Math Utilities" << endl;
-    
-    // Factorial Demo (Admin’s function)
-    cout<<"enter a number: "<<endl;
-    int x;
-    cin>>x;
-    cout << "Factorial(x): " << factorial(x) << endl;
-
-    // Prime Check Demo (Member B’s function)
-    // cout << "Is 13 prime? " << (isPrime(13) ? "Yes" : "No") << endl;
-
-    // Fibonacci Demo (’s function)
-    cout << "Fibonacci(7): " << fibonacci(7) << endl;
-
-    return 0;
 }
