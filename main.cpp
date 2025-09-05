1b9f533929d7ea5f0a95744e0df8a1d54d9eeb69
uday <udayasu.2004@gmail.com>
2025-09-05 19:59:43 +0530

    Added the Fibonacci function

--- main.cpp
+++ main.cpp
@@ -23,26 +23,13 @@ long long factorial(int n) {
 
 // ====================================
 // Function 3 - Fibonacci
-// Author: Member C
+// Author: Ch Naga Uday kiran
 // ====================================
 // TODO: Implement fibonacci function here
 // int fibonacci(int n) { ... }
-
-
-int main() {
-    cout << "Driver Program - Math Utilities" << endl;
-    
-    // Factorial Demo (Admin’s function)
-    cout<<"enter a number: "<<endl;
-    int x;
-    cin>>x;
-    cout << "Factorial(x): " << factorial(x) << endl;
-
-    // Prime Check Demo (Member B’s function)
-    // cout << "Is 13 prime? " << (isPrime(13) ? "Yes" : "No") << endl;
-
-    // Fibonacci Demo (Member C’s function)
-    // cout << "Fibonacci(7): " << fibonacci(7) << endl;
-
-    return 0;
+int fibonacci(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
